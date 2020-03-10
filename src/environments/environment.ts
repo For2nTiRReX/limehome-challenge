6a4a10c99464e279cf5d1e2f24e7698cc36451cf
bogdan <bogdkovalov@gmail.com>
2020-03-10 02:57:31 +0200

    init proj

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hereApiKey: "RkuUGIRiKrv2--11A8S6BAui6SSHt26zH7zExLkXxy8"
 };
 
 /*
